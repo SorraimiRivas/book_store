3e3412101e2fc13d09094523d5f9ae6ce11cf0be
emanuelprisker <emanuelprisker@gmail.com>
2022-02-11 23:50:11 -0300

    Se añade BookControllerTest

---
+++ src/test/java/com/onlinestore/kodigonlinestore/controller/BookControllerTest.java
@@ -0,0 +1,24 @@
+package com.onlinestore.kodigonlinestore.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookControllerTest {
+
+    @Test
+    void getAllCustomer() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
